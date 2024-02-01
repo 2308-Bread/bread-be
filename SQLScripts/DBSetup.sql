0f0a504e379e6c2cefa5a8a05869b2a9fb5c528d
Eric Belongea <eric.belongea@gmail.com>
2024-01-31 22:54:04 -0700

    Creates db with user controller and model

---
+++ SQLScripts/DBSetup.sql
@@ -0,0 +1,29 @@
+-- 1/31/24
+
+CREATE DATABASE BreadApiDatabase
+GO
+
+USE BreadApiDatabase
+GO
+
+CREATE TABLE Users
+(
+    UserId INT IDENTITY(1, 1) PRIMARY KEY
+    , FirstName NVARCHAR(50)
+    , LastName NVARCHAR(50)
+    , Email NVARCHAR(50)
+);
+
+CREATE TABLE Breads
+(
+    BreadId INT IDENTITY(1, 1) PRIMARY KEY
+    , Name NVARCHAR(50)
+    , Recipe NVARCHAR(50)
+    , Description NVARCHAR(50)
+);
+
+CREATE TABLE UserBreads
+(
+    UserId INT
+    , BreadId INT
+);
