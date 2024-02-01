b390dc78e7e8dc7aa8c2440a62f3233605be0c22
Eric Belongea <eric.belongea@gmail.com>
2024-01-31 23:13:18 -0700

    Add gitignore and readme

--- obj/Debug/net8.0/bread_be.AssemblyInfo.cs
+++ obj/Debug/net8.0/bread_be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bread_be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eda29c37b685295f8419e3b0dee0b797ef57de5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e71a6b7c08db40f8e3b1b6ba04e20de94f15337")]
 [assembly: System.Reflection.AssemblyProductAttribute("bread_be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bread_be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
