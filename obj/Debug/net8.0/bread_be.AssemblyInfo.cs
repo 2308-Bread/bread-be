4eda29c37b685295f8419e3b0dee0b797ef57de5
Eric Belongea <eric.belongea@gmail.com>
2024-01-31 23:01:16 -0700

    Install Dapper

--- obj/Debug/net8.0/bread_be.AssemblyInfo.cs
+++ obj/Debug/net8.0/bread_be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bread_be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0a504e379e6c2cefa5a8a05869b2a9fb5c528d")]
 [assembly: System.Reflection.AssemblyProductAttribute("bread_be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bread_be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
