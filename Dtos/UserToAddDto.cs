335028f5416c6c01a69350bcca9a2805336b3bcf
Eric Belongea <eric.belongea@gmail.com>
2024-02-09 11:35:22 -0700

    Builds webapp and starts deployment process

--- Dtos/UserToAddDto.cs
+++
@@ -1,9 +0,0 @@
-namespace BreadAPI.Dtos
-{
-    public class UserToAddDto
-    {
-        public string FirstName {get; set;} = "";
-        public string LastName {get; set;} = "";
-        public string Email {get; set;} = "";
-    }
-}
--- Models/Country.cs
+++
@@ -1,9 +0,0 @@
-namespace BreadAPI.Models
-{
-  public class Country
-  {
-    public int CountryId {get;set;}
-    public string Name {get;set;} = "";
-    public string Description {get;set;} = "";
-  }
-}
